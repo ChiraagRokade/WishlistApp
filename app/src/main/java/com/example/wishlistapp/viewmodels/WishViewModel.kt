54642fc83aac652b2437056d35e33fd93f4244b7
Chiraag Rokade <94799807+ChiraagRokade@users.noreply.github.com>
2025-01-28 16:57:59 +0530

    create graph class

--- app/src/main/java/com/example/wishlistapp/viewmodels/WishViewModel.kt
+++ app/src/main/java/com/example/wishlistapp/viewmodels/WishViewModel.kt
@@ -4,8 +4,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.wishlistapp.data.Wish
+import com.example.wishlistapp.data.WishRepository
+import kotlinx.coroutines.launch
 
-class WishViewModel:ViewModel() {
+class WishViewModel(
+    private val wishRepository: WishRepository
+):ViewModel() {
     var wishTitleState by mutableStateOf("")
     var wishDescriptionState by mutableStateOf("")
 
@@ -16,4 +22,35 @@ class WishViewModel:ViewModel() {
     fun onWishDescriptionChanged(newDescription:String){
         wishDescriptionState = newDescription
     }
+
+    lateinit var getAllWishes: List<Wish>
+
+    init {
+        viewModelScope.launch {
+            getAllWishes = wishRepository.getWished()
+        }
+    }
+
+    fun addWish(wish: Wish){
+        viewModelScope.launch {
+            wishRepository.addWish(wish)
+        }
+    }
+
+    fun updateWish(wish: Wish){
+        viewModelScope.launch {
+            wishRepository.updateWish(wish)
+        }
+    }
+
+    fun deleteWish(wish: Wish){
+        viewModelScope.launch {
+            wishRepository.deleteWish(wish)
+        }
+    }
+
+    fun getAWishById(id: Long): Wish?{
+        return wishRepository.getAWishById(id)
+    }
+
 }
