07310a98bc2a2cb4591d4233e2142eb49f58e112
Chiraag Rokade <94799807+ChiraagRokade@users.noreply.github.com>
2025-01-29 13:45:16 +0530

    add wish

--- app/src/main/java/com/example/wishlistapp/views/AddEditWishDetailView.kt
+++ app/src/main/java/com/example/wishlistapp/views/AddEditWishDetailView.kt
@@ -1,5 +1,6 @@
 package com.example.wishlistapp.views
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -10,15 +11,18 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
 import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Scaffold
 import androidx.compose.material.TextFieldDefaults
-import androidx.compose.material3.Scaffold
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
@@ -27,7 +31,9 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.wishlistapp.R
+import com.example.wishlistapp.data.Wish
 import com.example.wishlistapp.viewmodels.WishViewModel
+import kotlinx.coroutines.launch
 
 @Composable
 fun AddEditWishDetailView(
@@ -39,7 +45,13 @@ fun AddEditWishDetailView(
     val snackMessage = remember {
         mutableStateOf("")
     }
+
+    val scaffoldState = rememberScaffoldState()
+    val context = LocalContext.current
+    val scope = rememberCoroutineScope()
+
     Scaffold(
+        scaffoldState = scaffoldState,
         topBar = {
             AppBarView(
                 title = if (id == 0L) stringResource(id = R.string.add_wish) else stringResource(id = R.string.update_wish) ,
@@ -78,9 +90,25 @@ fun AddEditWishDetailView(
             Button(
                 onClick = {
                     if(viewModel.wishTitleState.isNotEmpty() && viewModel.wishDescriptionState.isNotEmpty()){
-                        // TODO: Update Wish
+                        if(id == 0L){
+                            // TODO: Add Wish
+                            viewModel.addWish(
+                                Wish(
+                                    title = viewModel.wishTitleState.trim(),
+                                    description = viewModel.wishDescriptionState.trim()
+                                )
+                            )
+                            Toast.makeText(context, "Wish Added", Toast.LENGTH_SHORT).show()
+                        }else {
+                            // TODO: Update Wish
+                        }
                     }else{
-                        // TODO: Add Wish
+                        // TODO: Enter fields for wish to be created
+                        snackMessage.value = "Enter fields to create a wish."
+                    }
+                    scope.launch {
+//                        scaffoldState.snackbarHostState.showSnackbar(snackMessage.value)
+                        navController.navigateUp()
                     }
                 }
             ){
